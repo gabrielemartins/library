c0168d65c4385b0972f82a1794b98775e6016e1a
Gabriele <gabriieele6@gmail.com>
2020-06-16 08:31:05 -0300

    feat(database): creating mongodb database connection
    Creating a connection between the library database in mongodb and the project with mongoose
    
    BREAKING CHANGE: From now the project can access the library database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/library", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(3333, () => {
   console.log("Server running at port 3333");

b481f93efcef9a2868b5277b1d93259f1b2681a4
Gabriele <gabriieele6@gmail.com>
2020-06-17 14:34:45 -0300

    feat(book schema): updating book schema to deal with the file upload
    updating book schema to save the cover url with the local url when the application is in the
    development stage; book schema is also updated for, when the user delete the file, it will also be
    deleted from AWS S3 or locally

--- backend/src/models/Book.js
+++ backend/src/models/Book.js
@@ -18,7 +18,11 @@ const BookSchema = new mongoose.Schema({
 
   summary: String,
 
-  cover: String,
+  cover: {
+    key: String,
+    name: String,
+    url: String,
+  },
 
   review: Number,
 
@@ -28,4 +32,36 @@ const BookSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+// Saving the cover url with the local url
+BookSchema.pre("save", function () {
+  if (!this.cover.url) {
+    this.cover.url = `${process.env.APP_URL}/files/${this.cover.key}`;
+  }
+});
+
+// Deleting the file from AWS S3
+BookSchema.pre("remove", function () {
+  if (process.env.STORAGE_TYPE === "s3") {
+    return s3
+      .deleteObject({
+        Bucket: process.env.BUCKET_NAME,
+        Key: this.cover.key,
+      })
+      .promise()
+      .then((response) => {
+        console.log(response.status);
+      })
+      .catch((response) => {
+        console.log(response.status);
+      });
+  }
+  //Deleting the file locally
+  else {
+    return promisify(fs.unlink)(
+      path.resolve(__dirname, "..", "..", "tmp", "uploads", this.cover.key)
+    );
+  }
+});
+
 mongoose.model("Book", BookSchema);

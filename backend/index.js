98b26862f1bcee297db3c86824050fb6edc6ed7f
Gabriele <gabriieele6@gmail.com>
2020-06-16 21:51:53 -0300

    feat(index): updating index to access the routes
    BREAKING CHANGE: from now the application is capable of access the database models and the api
    routes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 const app = express();
+app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/library", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+requireDir("./src/models");
+
+app.use("/library", require("./src/routes"));
 
 app.listen(3333, () => {
   console.log("Server running at port 3333");

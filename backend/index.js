cc6f157f5dda906c214a2b4e8bb8c7eb2ae03240
Gabriele <gabriieele6@gmail.com>
2020-06-17 23:08:21 -0300

    build(index): adding packages, env variables and local file route
    adding required packages to the application operation, environment variables to mongodb access and
    the router that local files uploads will use
    
    BREAKING CHANGE: from now the application is able to access environments variables and the files
    upload locally will have an access route

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,26 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
+const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
-app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/library", {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 requireDir("./src/models");
+
+//Route for access the local files
+app.use("/files", express.static(path.resolve(__dirname, "tmp", "uploads")));
 
 app.use("/library", require("./src/routes"));
 
